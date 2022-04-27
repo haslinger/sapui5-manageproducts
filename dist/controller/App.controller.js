84c5c72e4fe2a3c3d46266033de2db42907ebc36
Stefan Haslinger <stefan.haslinger@mittenin.at>
2022-04-27 12:14:07 +0000

    week 3, unit 3

---
+++ dist/controller/App.controller.js
@@ -0,0 +1,1 @@
+sap.ui.define(["./BaseController"],function(e){"use strict";return e.extend("opensap.manageproducts.manageproducts.controller.App",{onInit:function(){this.getView().addStyleClass(this.getOwnerComponent().getContentDensityClass())}})});
