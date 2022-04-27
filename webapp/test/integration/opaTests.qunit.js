2c887f28f9f9e3625b587ec0c7ceed6bed005edd
Stefan Haslinger <stefan.haslinger@mittenin.at>
2022-04-27 07:53:16 +0000

    project generated from template

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,8 @@
+/* global QUnit */
+
+sap.ui.require([
+	"opensap/manageproducts/manageproducts/test/integration/AllJourneys"
+], function() {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
